6b0189dd26271ff62bf680b3e42d0e35d9045e98
AnjaMajurec <amajurec277@gmail.com>
2025-01-23 20:36:55 +0100

    Zadatak-23.1.-person,company,contract

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,36 +1,74 @@
 package org.example;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
-import javax.swing.text.html.parser.Entity;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) {
         EntityManagerFactory emf= Persistence.createEntityManagerFactory("JpaExampleUnit");
         EntityManager em=emf.createEntityManager();
 
+                try {
+                    em.getTransaction().begin();
 
-        em.getTransaction().begin();
 
-        Osoba osoba1=em.find(Osoba.class,1);
-        if(osoba1!=null){
-            System.out.println("Ime:"+osoba1.getIme());
-            System.out.println("Prezime: "+osoba1.getPrezime());
-            em.remove(osoba1);
+                    Company company = new Company();
+                    company.setName("Podravka");
+
+                    Person person1 = new Person();
+                    person1.setName("Anja Majurec");
+
+                    Person person2 = new Person();
+                    person2.setName("Ivor Stojevski");
+
+                    Contract contract1 = new Contract();
+                    contract1.setStartDate(LocalDate.of(2025, 1, 2));
+                    contract1.setDurationInMonths(12);
+                    contract1.setSalary(new BigDecimal("2500"));
+                    contract1.setCompany(company);
+
+                    Contract contract2 = new Contract();
+                    contract2.setStartDate(LocalDate.of(2023, 3, 1));
+                    contract2.setDurationInMonths(24);
+                    contract2.setSalary(new BigDecimal("6000"));
+                    contract2.setCompany(company);
+
+                    Set<Person> personsForContract1 = new HashSet<>();
+                    personsForContract1.add(person1);
+                    personsForContract1.add(person2);
+
+                    Set<Person> personsForContract2 = new HashSet<>();
+                    personsForContract2.add(person1);
+
+                    contract1.setPersons(personsForContract1);
+                    contract2.setPersons(personsForContract2);
+
+
+                    em.persist(company);
+                    em.persist(person1);
+                    em.persist(person2);
+                    em.persist(contract1);
+                    em.persist(contract2);
+
+
+                    contract1.setSalary(new BigDecimal("3000"));
+                    em.merge(contract1);
+
+                    em.remove(contract2);
+
+                    em.getTransaction().commit();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        else{
-            System.out.println("Osoba ne postoji!");
-        }
-
-        em.getTransaction().commit();
 
 
-    }
-}
+
+
---
+++ src/main/java/org/example/Company.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+import java.util.*;
+@Entity
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "company")
+    private Set<Contract> contracts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Contract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(Set<Contract> contracts) {
+        this.contracts = contracts;
+    }
+}
